5d8a3b8df73c0b4be22669615ba1eea857d86a8b
Nick Sutterer <apotonick@gmail.com>
2016-07-29 10:53:18 +0200

    spec should be test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,6 @@ DB.connect
 require_relative "../application"
 
 require 'rack/test'
-Dir['./spec/support/*.rb'].each { |file| require file }
+Dir['./test/support/*.rb'].each { |file| require file }
 
 # require "match_json"
